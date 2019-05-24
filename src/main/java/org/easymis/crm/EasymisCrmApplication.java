f069843952f6684fd023cbbeb669327e3b21817e
tanyujie <13551259347@139.com>
2019-05-24 18:31:49 +0800

    init

---
+++ src/main/java/org/easymis/crm/EasymisCrmApplication.java
@@ -0,0 +1,13 @@
+package org.easymis.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasymisCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasymisCrmApplication.class, args);
+	}
+
+}
